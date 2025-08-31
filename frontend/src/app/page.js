61eadf598f97b4cb67af24adb306193504c6150d
luutanhung <luutanhung.dev@gmail.com>
2025-08-31 15:43:01 +0700

    feat(ui): add URL Shortening form with result display

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,70 @@
 "use client";
 
-import { useEffect } from "react";
+import { Button, Card, Form, Input, Typography } from "antd";
+import { useState } from "react";
+
+import { API_URL } from "@/config/environments";
+import api from "@/utils/axios";
+
+const { Text } = Typography;
 
 export default function HomePage() {
-  useEffect(() => {});
+  const [loading, setLoading] = useState(false);
+  const [shortCode, setShortCode] = useState(null);
+
+  const onFinish = async (values) => {
+    setLoading(true);
+    try {
+      const res = await api.post("/api/shorten", values);
+      setShortCode(res.data.short_code);
+    } catch (err) {
+      console.error(err?.response?.data?.message || "URL shortening failed!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen px-4">
+      <Card className="w-full max-w-md">
+        <Form name="shorten_url_form" layout="vertical" onFinish={onFinish}>
+          <Form.Item
+            label="Original URL"
+            name="original_url"
+            rules={[
+              { required: true, message: "Please enter the URL!" },
+              { type: "url", message: "Please enter a valid URL!" },
+            ]}
+          >
+            <Input placeholder="https://example.com" />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="w-full"
+              loading={loading}
+            >
+              Shorten URL
+            </Button>
+          </Form.Item>
+
+          {shortCode && (
+            <Form.Item>
+              <Text>
+                Shortened URL:{" "}
+                <a
+                  href={`${API_URL}/${shortCode}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {shortCode}
+                </a>
+              </Text>
+            </Form.Item>
+          )}
+        </Form>
+      </Card>
+    </div>
+  );
 }
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+import { API_URL } from "@/config/environments";
+
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
